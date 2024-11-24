2d1ea649c2dacf9d80745d93e4c53a106b7e53b1
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-24 14:43:01 -0600

    Initialize human and computer score

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Set player and computer score
+let humanScore = 0;
+let computerScore = 0;
+
 // Get random computer choice
 let getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
