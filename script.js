45650e52db72eda2d4e59caec607cf0b2fa65b56
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-24 15:10:17 -0600

    Create function to evaluate winner

--- script.js
+++ script.js
@@ -7,20 +7,44 @@ let getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     if (computerChoice === 1) {
-        return "Rock"
+        return "rock"
     } else if (computerChoice === 2) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-console.log(getComputerChoice());
-
 // Get player choice
-let getHumanChoice = prompt("Rock, Paper, or Scissors?");
+let getHumanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
 console.log(getHumanChoice);
 
 // Evaluate who the winner is
-//     increase score count
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "Computer wins! Paper beats Rock.";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return "Computer wins! Scissors beats Paper.";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "Computer wins! Rock beats Scissors.";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "Player wins! Paper beats Rock.";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "Player wins! Scissors beats Paper.";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "Player wins! Rock beats Scissors."
+    } else {
+        return "It's a tie!"
+    }
+}
+
+console.log(playRound(getHumanChoice, getComputerChoice()));
+
